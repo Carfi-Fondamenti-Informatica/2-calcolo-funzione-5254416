a4b462f13773cd5ec2252076d9da5ae91e4b8717
5254416 <100807837+5254416@users.noreply.github.com>
2022-04-03 17:21:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+    float a, b, x, y, z;
+  std::cout<<"Inserire quattro numeri reali \n";
+  cin >> a >> b >> x >> y;
+  if (x<0 && y>0) {
+      z= a*x - b*y;
+      std::cout << z;
+  } else if(x>=0 && y<=0) {
+      z=a*x*x-b*y;
+      std::cout << z;
+  } else {
+      z = a*x + b*y*y;
+      std::cout << z;
+  }
    return 0;
 }
